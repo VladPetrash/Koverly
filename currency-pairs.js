a7f46141f8250dd76b8169e83337da616c3ae671
VladPetrash <69577177+VladPetrash@users.noreply.github.com>
2022-10-13 15:10:43 +0300

    Update currency-pairs.js

--- currency-pairs.js
+++ currency-pairs.js
@@ -1,3 +1,35 @@
+//-------------------INPUT NUM VALIDATION---------------
+function validateNumber(event) {
+    var key = window.event ? event.keyCode : event.which;
+    if (event.keyCode === 8 || event.keyCode === 46) {
+        return true;
+    } else if ( key < 48 || key > 57 ) {
+        return false;
+    } else {
+        return true;
+    }
+};
+
+$(document).ready(function(){
+    $('#sendField').keypress(validateNumber);
+    
+    // Change currency logic
+$('.currency__dropdown__link').on('click',function(e){
+    e.preventDefault()
+    // close dropdown by click on item
+    $(".currency__dropdown-wrap.w-dropdown").triggerHandler("w-close.w-dropdown")
+    // get all data from clicked item
+    const flag = $(this).find('.currency-logo').text()
+    const currencyName = $(this).find('.toggle__text').text()
+    const selectedCurrencyLink = $(this).attr("href")
+    // set data to active item
+    $('#currency-logo').text(flag)
+    $('#currency-name').text(currencyName)
+    //set selected item link to button
+    $('#currencyLearnMore').attr("href", selectedCurrencyLink)
+})
+});
+
 // FORM (HOME / GLOBAL / PRICING PAGES)
 //-------------------FORM REDIRECT--------------------------
 let Webflow = window.Webflow || [];
